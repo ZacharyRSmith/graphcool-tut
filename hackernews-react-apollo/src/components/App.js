a0d0d34fcea73c10aace1c2a6179f967efede220
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-01-08 21:49:02 -0500

    feature: Paginate Links

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Redirect, Route } from 'react-router-dom'
 import React, { Component } from 'react'
 
 import CreateLink from './CreateLink'
@@ -14,10 +14,12 @@ class App extends Component {
         <Header />
         <div className='ph3 pv1 background-gray'>
           <Switch>
-            <Route exact path='/login' component={Login}/>
-            <Route exact path='/create' component={CreateLink}/>
-            <Route exact path='/' component={LinkList}/>
-            <Route exact path='/search' component={Search}/>
+            <Route exact path='/' render={() => <Redirect to='/new/1' />} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/create' component={CreateLink} />
+            <Route exact path='/search' component={Search} />
+            <Route exact path='/top' component={LinkList} />
+            <Route exact path='/new/:page' component={LinkList} />
           </Switch>
         </div>
       </div>
